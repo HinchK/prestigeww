0a183afcc15fa92ab393e22298643439f47e1445
hinchk <kasey.hinchman@gmail.com>
2016-07-02 23:42:07 -0700

    initial commit
    dash sendaquote internal [GLQ]

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+    <!-- To the right -->
+    <div class="pull-right hidden-xs">
+        v:pre01
+    </div>
+    <!-- Default to the left -->
+    <strong>Copyright © 2016 <a href="#">GLQUOTE</a>.</strong> All rights reserved.
+</footer>
